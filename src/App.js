79d586b7f4f1e80ef43c299a651745f85695ff0a
Gabriel Natan <gabriel--natan@live.com>
2022-01-10 16:25:10 -0300

    line chart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import {useState} from "react";
-import DevicesList from "./Components/devicesList";
-import Ticket from "./Components/Ticket";
+import LineChartRealTime from "./Components/LineChartRealTime";
 function App() {
   const [open,setOpen] = useState(false)
   return (
     <div className="App">
       <div style={{padding:20,margin:30,borderRadius:"10px",width:500,height:300,background:"#FFF"}}>
-        <DevicesList/>
+        <LineChartRealTime/>
       </div>
       <button onClick={()=>setOpen(true)}>Open</button>
     </div>
